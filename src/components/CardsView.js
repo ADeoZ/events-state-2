1103c6a3bc08280e4db3bc61d80d3b9fbabd60e3
Aleksey Denisov <deoz@yandex.ru>
2021-07-18 19:29:13 +0300

    Formatting fix

--- src/components/CardsView.js
+++ src/components/CardsView.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ShopCard from './ShopCard';
 import PropTypes from 'prop-types';
 
-export default function CardsView ({cards}) {
+export default function CardsView({ cards }) {
   return (
     <div className="CardsView">
       {cards.map((card) => {
         return (
-          <ShopCard card={card} key={card.name + card.color}/>
+          <ShopCard card={card} key={card.name + card.color} />
         );
       })}
     </div>
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -5,7 +5,7 @@ export default function ShopItem({ item }) {
   const background = { backgroundImage: 'url(' + item.img + ')' };
   return (
     <div className="ShopItem">
-      <div className="ShopItem-img" style={background}></div>
+      <div className="ShopItem-img" style={background} />
       <div className="ShopItem-name">{item.name}</div>
       <div className="ShopItem-color">{item.color}</div>
       <div className="ShopItem-price">${item.price}</div>
